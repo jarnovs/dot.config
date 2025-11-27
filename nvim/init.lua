93f43a153e7b2172e819182d0cdb2f2766c471e8
jarnov <jarnov@awork.kg>
2025-11-27 18:10:04 +0600

    feat: new

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,7 @@ vim.pack.add{
   { src = "https://github.com/sphamba/smear-cursor.nvim"},
   { src = "https://github.com/ThePrimeagen/harpoon"},
   { src = "https://github.com/mbbill/undotree"},
+  { src = "https://github.com/christoomey/vim-tmux-navigator"}
 }
 
 local builtin = require('telescope.builtin')
